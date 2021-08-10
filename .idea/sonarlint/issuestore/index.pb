716f94e153dca249cc720dd7e198af21671cae14
Juan Gabriel <jvelasquez@rapicredit.com>
2021-08-09 20:36:47 -0500

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+m
+=src/main/java/co/udea/hero/api/controller/HeroController.java,8/7/87d59f1fccbd8dd12a2780cf128688097f00d4be
+g
+7src/main/java/co/udea/hero/api/service/HeroService.java,f/7/f771abe77386b33b4d465d8c5d92c5c6255b19d6
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/co/udea/hero/api/service/HeroService.java
@@ -0,0 +1,69 @@
+package co.udea.hero.api.service;
+
+import co.udea.hero.api.exception.BusinessException;
+import co.udea.hero.api.model.Hero;
+import co.udea.hero.api.repository.HeroRepository;
+import co.udea.hero.api.util.Messages;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class HeroService {
+
+    private HeroRepository heroRepository;
+    private Messages messages;
+
+    public HeroService(HeroRepository heroRepository, Messages messages){
+        this.messages = messages;
+        this.heroRepository = heroRepository;
+    }
+
+    public List<Hero> getHeroes(){
+        List<Hero> heroesList = heroRepository.findAll();
+        return heroesList;
+    }
+
+    public Hero getHero(Integer id){
+        Optional<Hero> optionalHero = heroRepository.findById(id);
+        if(!optionalHero.isPresent()){
+            throw new BusinessException(messages.get("exception.data_not_found.hero"));
+        }
+        return optionalHero.get();
+    }
+
+    public Hero getHeroByName(String name){
+        Optional<Hero> optionalHero = heroRepository.findByName(name);
+        if(!optionalHero.isPresent()){
+            throw new BusinessException(messages.get("exception.data_not_found.hero"));
+        }
+        return optionalHero.get();
+    }
+
+    public Hero addHero(Hero hero){
+        Optional<Hero> optionalHero = heroRepository.findByName(hero.getName());
+        if(optionalHero.isPresent()){
+            throw new BusinessException(messages.get("exception.data_duplicate_name.hero"));
+        }
+        return heroRepository.save(hero);
+    }
+
+    public Hero updateHero(Hero hero){
+        Optional<Hero> optionalHero = heroRepository.findById(hero.getId());
+        if(optionalHero.isPresent()){
+            return heroRepository.save(hero);
+        }else{
+            throw new BusinessException(messages.get("exception.data_not_found.hero"));
+        }
+    }
+
+    public Hero deleteHero(Integer id){
+        Hero heroToDelete = this.getHero(id);
+        heroRepository.delete(heroToDelete);
+        return heroToDelete;
+    }
+
+
+}
+
